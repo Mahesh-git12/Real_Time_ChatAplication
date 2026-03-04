c1863b7d0eddad3c722421af4cd97d22fa8df98e
Maheshgit-12 <kokkantimahi89@gmail.com>
2026-03-04 13:42:06 +0530

    testing pipeline with server.js

--- backend/server.js
+++ backend/server.js
@@ -216,7 +216,7 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    server.listen(PORT, () => console.log(`Server is runninngg on the port ${PORT}`));
+    server.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
   })
   .catch(err => {
     console.error('MongoDB connection error:', err.message);
