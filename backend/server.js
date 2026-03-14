7fe4b80c966d43467efff7558c5bf944f2a9b237
Maheshgit-12 <kokkantimahi89@gmail.com>
2026-03-14 10:54:29 +0530

    new server file modification

--- backend/server.js
+++ backend/server.js
@@ -223,7 +223,7 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    server.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
+    server.listen(PORT, () => console.log(`Server is runninggg on the port ${PORT}`));
   })
   .catch(err => {
     console.error('MongoDB connection error:', err.message);
